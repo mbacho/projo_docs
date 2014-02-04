0f604a693a1485ae885ed2f3edad212e1589d46e
Chomba Ng'ang'a <choeringa@gmail.com>
2014-02-04 03:17:37 +0300

    Initial structure of project report

---
+++ src/report_tex/samplecode/test.py
@@ -0,0 +1,11 @@
+from sys import argv,exit
+
+def main():
+    """
+        Hello world! This is a docstring
+    """
+    print argv
+  
+if __name__ == '__main__':
+    main() #this is a comment
+
